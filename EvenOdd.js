b4080def2fd933c76a8f620d7958248d1cbd1351
Kunalsahuji <ksahu0103@gmail.com>
2025-03-26 21:54:01 +0530

    Add program to check if a number is even or odd

---
+++ EvenOdd.js
@@ -0,0 +1,11 @@
+// Write a program to check the given number is even or odd.
+let num = 11 //parseInt(prompt("Enter a number: "))
+
+if (num % 2 === 0) {
+    console.log(`${num} is an even number.`);
+} else {
+    console.log(`${num} is an odd number.`);
+}
+
+// Output: 10 is an even number.
+// Output: 11 is an odd number.
